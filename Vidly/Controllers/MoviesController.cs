5f8770cbd60504d73680318104c37f8063bc2d8a
Sai <saipriyatham22@gmail.com>
2016-07-17 15:57:47 -0400

    pageindex&sortby

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Sultan!" };
+            return View(movie);
+            
+        }
+        public ActionResult edit(int id)
+        {
+
+            return Content("id" + id);
+        }
+        public ActionResult Index(int? pageindex, string sortby)
+        {
+            if (!pageindex.HasValue)
+                pageindex = 1;
+            if (String.IsNullOrWhiteSpace(sortby))
+                sortby = "Name";
+            return Content(String.Format("pageindex={0}&sortby={1}",pageindex, sortby));
+        }
+    }
+}
